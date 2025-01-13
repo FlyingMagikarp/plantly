09c7e7162c5a7ca62aa66d6817c0309ed4f1afde
Alvaro Fernandez <alvaro.fernandez@transgourmet.ch>
2025-01-13 12:47:36 +0100

    spring init

---
+++ plantly-backend/src/main/java/com/magikarp/plantly_backend/PlantlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.magikarp.plantly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantlyBackendApplication.class, args);
+	}
+
+}
