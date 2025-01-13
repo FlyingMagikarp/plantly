9976da05da9c07ab8c476accf8f7fd8ff3748eaf
Alvaro Fernandez <alvaro.fernandez@transgourmet.ch>
2025-01-13 15:54:56 +0100

    prog

---
+++ plantly-backend/src/main/java/com/magikarp/plantly_backend/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.magikarp.plantly_backend.user;
+
+import com.magikarp.plantly_backend.user.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByName(String name);
+}
